94167172ba08cfbfa2cc687a751422c9f5e86799
Miaki <goldenation139@gmail.com>
2025-04-19 19:37:47 +0700

    Nambahin Button

---
+++ src/button.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function button (){
+    return( 
+        <>
+            <button className="min-w-[4rem] w-full sm:w-24 h-12 bg-blue-500 hover:bg-blue-700 cursor-pointer text-white text-5x1 font-semibold rounded flex items-center justify-center text-center px-1 m-5" id="button3">
+                  Trial Button
+            </button>
+        </>
+    )
+}
+
+export default button;
